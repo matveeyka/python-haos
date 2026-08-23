211695cd86a07a6465bef95d69300cea6ad854aa
Matthew <matthew@debian.domain>
2026-08-23 21:26:04 +0300

    still debugging

--- python-haos/app.py
+++ python-haos/app.py
@@ -1,6 +1,9 @@
 try:
     import eiscp
-    print("EISCP works!")
+
+    print("EISCP works!", flush=True)
+    print(f"EISCP location: {eiscp.__file__}", flush=True)
+
 except Exception as error:
     print(f"Something happened: {error}", flush=True)
 
