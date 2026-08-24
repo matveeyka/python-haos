8edbe2ff93c195930f8dc80445b79cb9ab2354b0
Matthew <matthew@debian.domain>
2026-08-24 12:38:28 +0300

    finally some authomatization

--- python-haos/app.py
+++ python-haos/app.py
@@ -1,9 +1,42 @@
+from flask import Flask, request, jsonify
 import eiscp
 
+app = Flask(__name__)
 receiver = eiscp.eISCP('192.168.98.127')
 
-try:
-    receiver.command('volume 55')
-    print("Check volume")
-except Exception as error:
-    print(f"Error: {error}")
+@app.route("/onkyo/power", methods=["POST"])
+def power():
+    data = request.get_json()
+    cond = data["power"]
+    
+    try:
+        receiver.command(f"power {cond}")
+        return jsonify({
+                "status":"success",
+                "power": f"{cond}"
+            })
+    except Exception as error:
+        return jsonify({
+                "error": error
+            })
+
+@app.route("/onkyo/volume", methods=["POST"])
+def set_volume():
+    data = request.get_json()
+    volume = float(data["volume"])
+
+    try:
+        receiver.command(
+            f"volume={int(volume * 2)}"
+        )
+
+        return jsonify({
+            "success": True,
+            "volume": volume
+        })
+    except Exception as error:
+        return jsonify({
+                "error": error
+            })
+
+app.run(host="0.0.0.0", port=1111)
